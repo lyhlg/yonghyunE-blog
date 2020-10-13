22390394d35cf9b9a187a79186088e2f95cab7d3
Yonghyun, Lee <lyhlg0201@gmail.com>
2020-10-13 16:10:03 +0900

    Fixed bug incorrect access of keyword list

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-export function Head({ description, lang, meta, keywords, title }) {
+export function Head({ description, lang, meta, keywords = [], title }) {
   return (
     <StaticQuery
       query={detailsQuery}
